fdf5a8ac37f155a15445cc40134f883154855cb1
MIKEY EDMAR <cgcom.bsolution@gmail.com>
2026-08-12 17:40:06 +0800

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDZ5Sy_0bex1XQnbEwnxAjxQhDWh4GcAoc",
+  authDomain: "kapirata-basketball-challenge.firebaseapp.com",
+  projectId: "kapirata-basketball-challenge",
+  storageBucket: "kapirata-basketball-challenge.firebasestorage.app",
+  messagingSenderId: "385122231763",
+  appId: "1:385122231763:web:7278ab76f319017a623b26"
+};
